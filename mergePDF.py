a7829ccfd4c049b80aa01cdbbc91cb45d3b2040e
manparma <parmar.c.manish@gmail.com>
2021-02-06 10:48:27 -0800

    mergePDF

---
+++ mergePDF.py
@@ -0,0 +1,16 @@
+# pip install PyPDF2
+
+import os
+from PyPDF2 import PdfFileMerger
+
+source_dir = os.getcwd()
+
+merger = PdfFileMerger()
+
+for item in os.listdir(source_dir):
+    if item.endswith('pdf'):
+        merger.append(item)
+
+merger.write('./Output/merged_PDF.pdf')
+merger.close()
+
